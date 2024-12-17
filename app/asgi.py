e4087ef11150d0c5e1b350ab074febf50b2d9b2c
Gokul Raj <serajgokul19@gmail.com>
2024-12-17 23:16:05 +0530

    consumers and routing

--- app/asgi.py
+++ app/asgi.py
@@ -1,21 +1,20 @@
-"""
-ASGI config for app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http":get_asgi_application()
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
