e59e9a13d26c8c2bcef141dd757273809137fbd6
Gokul Raj <serajgokul19@gmail.com>
2024-12-20 23:25:48 +0530

    consumers added

--- app/settings.py
+++ app/settings.py
@@ -72,6 +72,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 ASGI_APPLICATION ='app.asgi.application'
+CHANNEL_LAYERS = {
+    "default":{
+    "BACKEND":"channels_redis.core.RedisChannelLayer",
+    "CONFIG":{
+        "hosts":[("127.0.0.1", 6379)],
+    }
+    }
+    }
+
 
 
 # Database
